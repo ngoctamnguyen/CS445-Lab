8eb76f9dc2a593c87a0bf35a837fadcddcb4ddf9
ngoctamnguyen <ngocnguyen@miu.edu>
2022-04-28 08:33:35 -0500

    commit lab21.js

---
+++ Lab2/lab21.js
@@ -0,0 +1,31 @@
+/* Re-write the following code using TypeScript Class syntax. Try to be as explicit as possible and add Types to everything you can.
+ When you are done, transpile the TS code to JS code and inspect the JS code.
+let baseObject = { 
+	width: 0, 
+	length: 0 
+}; 
+let rectangle = Object.create(baseObject); 
+
+rectangle.width = 5; 
+rectangle.length = 2; 
+
+rectangle.calcSize = function() { 
+	return this.width * this.length; 
+}; 
+
+console.log(rectangle.calcSize()); // 10 */
+
+let baseObject = { 
+	width: 0, 
+	length: 0 
+}; 
+let rectangle = Object.create(baseObject); 
+
+rectangle.width = 5; 
+rectangle.length = 2; 
+
+rectangle.calcSize = function() { 
+	return this.width * this.length; 
+}; 
+
+console.log(rectangle.calcSize()); // 10
